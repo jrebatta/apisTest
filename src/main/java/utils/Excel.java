c4a69809c004e2b67ac060f0ce59fee9c049c844
jrebatta <i201610109@cibertec.edu.pe>
2023-02-03 23:33:18 -0500

    final

--- src/main/java/utils/Excel.java
+++ src/main/java/utils/Excel.java
@@ -12,40 +12,6 @@ public class Excel {
 
     public String filepathX = "data/dataTest.xlsx";
 
-//    public String searchCampo (String sheetName, String nameCampo, String CP) {
-//        String Resultado = "";
-//        try {
-//
-//            File file = new File(filepathX);
-//
-//            FileInputStream inputStream = new FileInputStream(file);
-//
-//            XSSFWorkbook newWorkbook = new XSSFWorkbook(inputStream);
-//
-//            XSSFSheet newSheet = newWorkbook.getSheet(sheetName);
-//
-//            int fila_CP = searchPositionCP(sheetName, CP);
-//
-//            for (int i = 0; i < 1; i++) {
-//                XSSFRow row = newSheet.getRow(i);
-//
-//                for (int j = 0; j < row.getLastCellNum(); j++) {
-//
-//                    if(row.getCell(j).getStringCellValue().equals(nameCampo))
-//                    {
-//                        Resultado = getCellValue(sheetName, fila_CP, j);
-//                    }
-//                }
-//            }
-//            return Resultado;
-//        }catch (IOException e)
-//        {
-//            e.printStackTrace();
-//            return Resultado;
-//        }
-//
-//    }
-
     public String getCellValue(String sheetName, int rowNumber, int cellNumber) throws IOException {
 
         File file = new File(filepathX);
@@ -64,66 +30,6 @@ public class Excel {
 
     }
 
-//    public int searchPositionCP(String sheetName, String CP) throws IOException
-//    {
-//        int Resultado = 0;
-//        File file = new File(filepathX);
-//
-//        FileInputStream inputStream = new FileInputStream(file);
-//
-//        XSSFWorkbook newWorkbook = new XSSFWorkbook(inputStream);
-//
-//        XSSFSheet newSheet = newWorkbook.getSheet(sheetName);
-//
-//        int rowCount = newSheet.getLastRowNum() - newSheet.getFirstRowNum();
-//        for (int i = 1; i <= rowCount; i++) {
-//            XSSFRow row = newSheet.getRow(i);
-//            String[] codigo = CP.split("_");
-//            String codigoPMICE = codigo[0];
-//            if(row.getCell(0).getStringCellValue().equals(codigoPMICE))
-//            {
-//                Resultado = i;
-//                break;
-//            }
-//
-//        }
-//        return Resultado;
-//    }
-
-//    public void writeCellValue(String sheetName, String nameCampo, String CP, String valor){
-//
-//        try {
-//
-//            File file = new File(filepathX);
-//
-//            FileInputStream inputStream = new FileInputStream(file);
-//
-//            XSSFWorkbook newWorkbook = new XSSFWorkbook(inputStream);
-//
-//            XSSFSheet newSheet = newWorkbook.getSheet(sheetName);
-//
-//            int fila_CP = searchPositionCP(sheetName, CP);
-//
-//            for (int i = 0; i < 1; i++) {
-//                XSSFRow row = newSheet.getRow(i);
-//
-//                for (int j = 0; j < row.getLastCellNum(); j++) {
-//
-//                    if(row.getCell(j).getStringCellValue().equals(nameCampo))
-//                    {
-//                        setCellValue(sheetName, fila_CP, j,valor);
-//                    }
-//
-//
-//                }
-//            }
-//        }catch (IOException e)
-//        {
-//            e.printStackTrace();
-//        }
-//
-//    }
-
     public void setCellValue(String sheetName, int rowNumber, int cellNumber, String valor) throws IOException {
 
         File file = new File(filepathX);
@@ -137,12 +43,10 @@ public class Excel {
 
         Cell cell = null;
 
-        //Retrieve the row and check for null
         XSSFRow sheetrow = newSheet.getRow(rowNumber);
         if(sheetrow == null){
             sheetrow = newSheet.createRow(rowNumber);
         }
-        //Update the value of cell
         cell = sheetrow.getCell(cellNumber);
         if(cell == null){
             cell = sheetrow.createCell(cellNumber);
